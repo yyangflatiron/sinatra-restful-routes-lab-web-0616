c0a157d602ee94761b60d21682c354b877c0aea8
yyangflatiron <yichen.yang@flatironschool.com>
2016-07-01 01:36:30 -0400

    Done.

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ group :test do
   gem 'pry'
   gem 'pry-nav'
   gem 'capybara'
+  gem 'shotgun'
   gem 'rack-test'
   gem 'database_cleaner', git: 'https://github.com/bmabey/database_cleaner.git'
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,50 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-  get '/' do
+
+  get '/recipes' do
+    @recipes=Recipe.all
     erb :index
   end
+
+  #new recipe
+  get '/recipes/new' do
+    erb :new
+  end
+
+  #create to save
+  post '/recipes' do
+    @recipe = Recipe.new(params)
+    @recipe.save
+    redirect to '/recipes/#{@recipe.id}'
+  end
+
+  #show
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+
+  #edit part 1
+  get '/recipes/:id/edit' do
+    @recipe=Recipe.find(params[:id])
+    erb :edit
+  end
+
+  #edit part 2
+  patch '/receipts/:id' do
+    @recipe=Recipe.find(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    erb :show
+  end
+
+  delete '/recipes/:id/delete' do
+    @recipe = Recipe.find(params[:id])
+    @recipe.delete
+    erb :index
+  end
+
 end
